f31f17850689391229377ccca4df7093c3798cbd
Abady <gamersspeaks@gmail.com>
2019-01-19 15:15:35 +0300

    better now!

--- src/commands/info/ping.js
+++ src/commands/info/ping.js
@@ -1,11 +1,19 @@
 const { Command } = require('discord-akairo');
+const { stripIndents } = require('common-tags');
+
+const RESPONSES = [
+	'Try again lover ~_~',
+	'Nope',
+	'5$ and I\'ll show you the ping?',
+	stripIndents`:ping_pong: Pong! \`$(ping)ms\`
+		Heartbeat: \`$(heartbeat)ms\``
+];
 
 class PingCommand extends Command {
 	constructor() {
 		super('ping', {
 			aliases: ['ping'],
-			cooldown: 5000,
-			ratelimit: 1,
+			ratelimit: 2,
 			category: 'info',
 			channel: 'guild',
 			description: {
@@ -14,9 +22,17 @@ class PingCommand extends Command {
 		});
 	}
 
+	/**
+	 *
+	 * @param {import('discord.js').Message} message
+	 */
 	async exec(message) {
 		const msg = await message.channel.send(`Pinging...`);
-		msg.edit(`:ping_pong: Pong! ${Date.now() - msg.createdTimestamp}ms`);
+		return msg.edit(
+			RESPONSES[Math.floor(Math.random() * RESPONSES.length)]
+				.replace('$(ping)', ((msg.editedTimestamp || msg.createdTimestamp) - (message.editedTimestamp || message.createdTimestamp)).toString())
+				.replace('$(heartbeat)', Math.round(this.client.ws.ping).toString())
+		);
 	}
 }
 
