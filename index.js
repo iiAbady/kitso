a69ef7f5588292f09f45b089a33e2933ac807bb0
Abady <gamersspeaks@gmail.com>
2019-01-19 03:28:39 +0300

    chore: let unhandledRejection handling on index.js for now.

--- index.js
+++ index.js
@@ -4,5 +4,7 @@ const client = new KitsoClient();
 client
 	.on('error', err => client.logger.error(`[Client Error]\n${err.stack}`))
 	.on('warn', warn => client.logger.warn(`[Client Warning]\n${warn}`));
+process
+	.on('unhandledRejection', error => client.logger.error(`[Process Unhandled Rejection]\n${error}`));
 
 client.start();
