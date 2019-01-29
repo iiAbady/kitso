2387a110a255e584bd6532611bed2dc45d48c967
Abady <gamersspeaks@gmail.com>
2019-01-29 21:09:21 +0300

    chore: better consoling

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const KitsoClient = require('./src/structures/client');
 const client = new KitsoClient({ owner: process.env.OWNERS, token: process.env.TOKEN });
 
 client
-	.on('error', err => client.logger.error(`[Client Error]\n${err.stack}`))
-	.on('warn', warn => client.logger.warn(`[Client Warning]\n${warn}`));
+	.on('error', err => client.logger.error(`[Client Error] ${err.stack}`))
+	.on('warn', warn => client.logger.warn(`[Client Warning] ${warn}`));
 
 client.start();
