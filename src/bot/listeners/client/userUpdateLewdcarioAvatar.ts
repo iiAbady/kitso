82bde7645688608aa3311103ad9794d6ea275b5c
Abady <gamersspeaks@gmail.com>
2019-02-10 18:08:54 +0300

    ok deleted un needed things.

--- src/bot/listeners/client/userUpdateLewdcarioAvatar.ts
+++ src/bot/listeners/client/userUpdateLewdcarioAvatar.ts
@@ -1,20 +1,20 @@
-import { Listener } from 'discord-akairo';
-import { User } from 'discord.js';
+// import { Listener } from 'discord-akairo';
+// import { User } from 'discord.js';
 
-export default class UserUpdateLewdcarioAvatarListener extends Listener {
-	public constructor() {
-		super('userUpdateLewdcarioAvatar', {
-			emitter: 'client',
-			event: 'userUpdate',
-			category: 'client'
-		});
-	}
+// export default class UserUpdateLewdcarioAvatarListener extends Listener {
+// 	public constructor() {
+// 		super('userUpdateLewdcarioAvatar', {
+// 			emitter: 'client',
+// 			event: 'userUpdate',
+// 			category: 'client'
+// 		});
+// 	}
 
-	public async exec(oldUser: User, newUser: User) {
-		if (oldUser.id === '84484653687267328' && newUser.id === '84484653687267328') {
-			if (oldUser.displayAvatarURL() !== newUser.displayAvatarURL()) {
-				this.client.prometheus.lewdcarioAvatarCounter.inc();
-			}
-		}
-	}
-}
+// 	public async exec(oldUser: User, newUser: User) {
+// 		if (oldUser.id === '84484653687267328' && newUser.id === '84484653687267328') {
+// 			if (oldUser.displayAvatarURL() !== newUser.displayAvatarURL()) {
+// 				this.client.prometheus.lewdcarioAvatarCounter.inc();
+// 			}
+// 		}
+// 	}
+// }
--- src/bot/structures/Database.ts
+++ src/bot/structures/Database.ts
@@ -1,16 +1,17 @@
 import { ConnectionManager } from 'typeorm';
 import { Setting } from '../models/Settings';
 import { Tag } from '../models/Tags';
-import { RoleState } from '../models/RoleStates';
-import { Case } from '../models/Cases';
-import { Reminder } from '../models/Reminders';
+// TODO: add these in future
+// import { RoleState } from '../models/RoleStates';
+// import { Case } from '../models/Cases';
+// import { Reminder } from '../models/Reminders';
 
 const connectionManager = new ConnectionManager();
 connectionManager.create({
 	name: 'kitso',
 	type: 'postgres',
 	url: process.env.DATABASE_URL,
-	entities: [Setting, Tag, RoleState, Case, Reminder]
+	entities: [Setting, Tag]
 });
 
 export default connectionManager;
