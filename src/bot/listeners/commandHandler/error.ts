c53d46660578f82573f6e9a21b84c157d7ccc4be
Abady <gamersspeaks@gmail.com>
2019-02-12 17:21:32 +0300

    k

--- src/bot/listeners/commandHandler/error.ts
+++ src/bot/listeners/commandHandler/error.ts
@@ -1,6 +1,13 @@
 import { Listener, Command } from 'discord-akairo';
 import { Message } from 'discord.js';
+import { emojis } from '../../util/index';
+const { shocked, thumbsUp1, thumbsUp2 } = emojis;
 const Raven = require('raven'); // tslint:disable-line
+const RESPONSES: string[] = [
+	`${shocked} W-What?!?! That was unexpected. (Error: !{err})`,
+	`${thumbsUp1} Alrrightt! I'am going to fix this asap. (Error: !{err})`,
+	`${thumbsUp2} Thank's for finding this bug for me, I'am going to slay it. (Error: !{err})`
+];
 
 export default class CommandErrorListener extends Listener {
 	public constructor() {
@@ -11,7 +18,7 @@ export default class CommandErrorListener extends Listener {
 		});
 	}
 
-	public exec(error: Error, message: Message, command: Command) {
+	public async exec(error: Error, message: Message, command: Command) {
 		this.client.logger.error(`[COMMAND ERROR] ${error.message}`, error.stack);
 		Raven.captureBreadcrumb({
 			message: 'command_errored',
@@ -37,5 +44,8 @@ export default class CommandErrorListener extends Listener {
 			}
 		});
 		Raven.captureException(error);
+		return message.util!.send(
+			RESPONSES[Math.floor(Math.random() * RESPONSES.length)].replace('!{err}',  `${command.id}${error.message.length + command.id.length}`)
+		);
 	}
 }
--- src/bot/util/index.ts
+++ src/bot/util/index.ts
@@ -1,4 +1,12 @@
-export const emojis = {
+interface Emojis {
+	closedEyes: '<:kitsoOwO:539847502866808843>';
+	shocked: '<:kitsoShocked:539848639225266176>';
+	thumbsUp1: '<:kitsoThumbsUp:539850864714579978>';
+	thumbsUp2: '<:kitsoThumbsUp2:539848759492739076>';
+	thumbsoUpo: '<:kitsoThumbsoUpo:544263946979246129>';
+}
+
+export const emojis: Emojis = {
 	closedEyes: '<:kitsoOwO:539847502866808843>',
 	shocked: '<:kitsoShocked:539848639225266176>',
 	thumbsUp1: '<:kitsoThumbsUp:539850864714579978>',
