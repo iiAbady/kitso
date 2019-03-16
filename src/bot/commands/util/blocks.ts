bbee66b949d20f93aa8fbce01b4b0fef18718208
Abady <gamersspeaks@gmail.com>
2019-03-17 00:19:42 +0300

    fix: embed sending spam

--- src/bot/commands/util/blocks.ts
+++ src/bot/commands/util/blocks.ts
@@ -30,16 +30,25 @@ export default class Searchcommand extends Command {
 				const player =  await blocksmc.player(query);
 				if(!player || !player.length) return message.util!.send("Looks like your query username doesn't even exist, you wasted my time!");
 				const embed = new MessageEmbed()
-								.setColor('BLUE');
+								.setColor('BLUE')
+								.setAuthor(query, `https://minotar.net/helm/${query}`, `https://blocksmc.com/player/${query}`);
 // tslint:disable-next-line: promise-function-async
-				player.map((g: { game: any; stats: { Kills: any; Deaths: any; Played: any; Points: any; Blocks: any; Eggs: any; Wins: any; FireWorks: any; Crates: any; DMs: any; Sponges: any; Beds: any; Rounds: any; Goals: any; }; }) => {
-					embed.addField(g.game,
-`
-${g.stats.Kills ? `Kills: ${g.stats.Kills} ` : ''}
+				player.map(async (g: { game: any; stats: { Kills: any; Deaths: any; Played: any; Points: any; Blocks: any; Eggs: any; Wins: any; FireWorks: any; Crates: any; DMs: any; Sponges: any; Beds: any; Rounds: any; Goals: any; }; }) => {
+					embed.addField(g.game, `${g.stats.Kills ? `Kills: ${g.stats.Kills} ` : ''}
 ${g.stats.Deaths ? `Deaths: ${g.stats.Deaths} ` : ''}
 ${g.stats.Played ? `Played: ${g.stats.Played} ` : ''}
 ${g.stats.Points ? `Points: ${g.stats.Points} ` : ''}
-`, true);
+${g.stats.Blocks ? `Blocks: ${g.stats.Blocks} ` : ''}
+${g.stats.Eggs ? `Eggs: ${g.stats.Eggs} ` : ''}
+${g.stats.Wins ? `Wins: ${g.stats.Wins} ` : ''}
+${g.stats.FireWorks ? `FireWorks: ${g.stats.FireWorks} ` : ''}
+${g.stats.Crates ? `Crates: ${g.stats.Crates} ` : ''}
+${g.stats.DMs ? `DMs: ${g.stats.DMs} ` : ''}
+${g.stats.Sponges ? `Sponges: ${g.stats.Sponges} ` : ''}
+${g.stats.Beds ? `Beds: ${g.stats.Beds} ` : ''}
+${g.stats.Rounds ? `Rounds: ${g.stats.Rounds} ` : ''}
+${g.stats.Goals ? `Goals: ${g.stats.Goals} ` : ''}
+`);
 // tslint:disable-next-line: indent
 			  return message.util!.send(embed);
 				});
