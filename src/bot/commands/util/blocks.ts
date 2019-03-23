cea997a4782b345280f42769ef7a7d9fe87ce6ad
Abady <gamersspeaks@gmail.com>
2019-03-23 18:08:09 +0300

    chore: update blocks to 2.4.3

--- src/bot/commands/util/blocks.ts
+++ src/bot/commands/util/blocks.ts
@@ -31,9 +31,11 @@ export default class Searchcommand extends Command {
 				if(!player) return message.util!.send("Looks like your query username doesn't even exist, you wasted my time!");
 				const embed = new MessageEmbed()
 								.setColor('BLUE')
-								.setAuthor(`[${player.rank}] ${query}`, `https://minotar.net/helm/${query}`, `https://blocksmc.com/player/${query}`);
+								.setAuthor(`[${player.rank}] ${query}`, `https://minotar.net/helm/${query}`, `https://blocksmc.com/player/${query}`)
+								.setDescription(`**Hours: ${player.timePlayed}**`);
 				player.games.map(async (g: { game: any; stats: { Kills: any; Deaths: any; Played: any; Points: any; Blocks: any; Eggs: any; Wins: any; FireWorks: any; Crates: any; DMs: any; Sponges: any; Beds: any; Rounds: any; Goals: any; }; }) => {
-					embed.addField(g.game, `${g.stats.Kills ? `Kills: ${g.stats.Kills} ` : ''}
+					embed.addField(g.game,
+`${g.stats.Kills ? `Kills: ${g.stats.Kills} ` : ''}
 ${g.stats.Deaths ? `Deaths: ${g.stats.Deaths} ` : ''}
 ${g.stats.Played ? `Played: ${g.stats.Played} ` : ''}
 ${g.stats.Points ? `Points: ${g.stats.Points} ` : ''}
