c215a62f77b550bd8a35e0a7e26be4ed72a8bac0
Abady <gamersspeaks@gmail.com>
2019-03-24 22:00:02 +0300

    fix: missing repo

--- src/bot/structures/Database.ts
+++ src/bot/structures/Database.ts
@@ -1,9 +1,7 @@
 import { ConnectionManager } from 'typeorm';
 import { Setting } from '../models/Settings';
 import { Tag } from '../models/Tags';
-// TODO: add these in future
-// import { RoleState } from '../models/RoleStates';
-// import { Case } from '../models/Cases';
+import { Case } from '../models/Cases';
 import { Reminder } from '../models/Reminders';
 
 const connectionManager = new ConnectionManager();
@@ -11,7 +9,7 @@ connectionManager.create({
 	name: 'kitso',
 	type: 'postgres',
 	url: process.env.DATABASE_URL,
-	entities: [Setting, Tag, Reminder]
+	entities: [Setting, Tag, Reminder, Case]
 });
 
 export default connectionManager;
