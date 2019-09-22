9ac97051123d073cddbd92c2296b5992bcbd3037
Abady <gamersspeaks@gmail.com>
2019-09-22 04:40:25 +0300

    fix: env

--- src/kitso.ts
+++ src/kitso.ts
@@ -2,8 +2,7 @@ import 'reflect-metadata';
 import KitsoClient from './bot/client/KitsoClient';
 import { Logger } from 'winston';
 import './env';
-// TODO: make this like the old way: process.env.OWNERS only
-const client = new KitsoClient({ owner: ['171259176029257728', '298732816995319809', '323231064157847559'], token: process.env.TOKEN });
+const client = new KitsoClient({ owner: process.env.OWNERS!.split(','), token: process.env.TOKEN });
 
 client
 	.on('error', (err): Logger => client.logger.error(`[CLIENT ERROR] ${err.message}`, err.stack))
