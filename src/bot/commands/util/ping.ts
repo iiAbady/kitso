01d25023e68548270c3c00d1d10997e6d4c60806
xRokz <38587835+xrokz@users.noreply.github.com>
2020-04-26 23:25:25 +0300

    Update ping.ts

--- src/bot/commands/util/ping.ts
+++ src/bot/commands/util/ping.ts
@@ -1,5 +1,6 @@
-import { Command } from 'discord-akairo';
+import { Command } from 'discord-akairo'; 
 import { Message } from 'discord.js';
+
 import { stripIndents } from 'common-tags';
 
 const RESPONSES: string[] = [
